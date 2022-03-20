ff2f4fb7f18afc2126b3a7f9400dcfe2f3553c47
lbmadesia <lalbabu@aresourcepool.com>
2022-03-20 22:14:07 +0530

    changed

--- routes/web.php
+++ routes/web.php
@@ -15,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
-
-
+})->name('dashboard');
 
 Route::get('lang/{lang}', 'LanguageController@swap');
 
